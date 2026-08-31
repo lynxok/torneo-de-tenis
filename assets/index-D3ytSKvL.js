5a19dbca994630d21b2da0e94b6285bcf233d2eb
Ignacio Valente <valenteignacio@example.com>
2026-08-31 10:04:10 -0300

    release: v1.6.4 - Modo TV Broadcast para Smart TVs con alcance por sede y Notificaciones Push PWA

--- assets/index-D3ytSKvL.js
+++
@@ -1,1 +0,0 @@
-import{a6 as r}from"./index-DGQidk_Z.js";var o=r();export{o as r};
--- smash-tennis-manager/public/sw.js
+++ smash-tennis-manager/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'smash-tennis-v1.6.0';
+const CACHE_NAME = 'smash-tennis-v1.6.4';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
